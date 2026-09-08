a45107c4d103d70b15a0ccc80f590da1931ae6b5
Santo Foti <santo.foti@edu.unife.it>
2026-09-08 11:13:04 +0200

    esportate funzioni

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,4 @@
 from app.services.discount import applica_sconto
 from app.services.order_state import cambia_stato_ordine, richiedi_rimborso, TRANSIZIONI_VALIDE
 from app.services.review import crea_recensione, aggiorna_recensione, elimina_recensione
+from app.services.checkout import checkout, RigaCarrello
